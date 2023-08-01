19fdc86dd9ac276dbc5a26d197ffe33434f85bd8
abhicodes13 <68747686+abhicodes13@users.noreply.github.com>
2023-08-01 16:18:01 +0000

    Updated TourCard with an API to display data

--- src/components/TourCard.jsx
+++ src/components/TourCard.jsx
@@ -5,14 +5,14 @@ import Typography from '@mui/material/Typography';
 import { AccessTime } from '@mui/icons-material';
 import Rating from '@mui/material/Rating';
 
-const TourCard = () => {
+const TourCard = ({tour}) => {
   return (
     <Grid item xs={4}>
     <Paper elevation={1}>
-    <img src="https://tcproduction.blob.core.windows.net/media/%7B240f8b72-1159-4fd3-a150-0a837f50ba4a%7D.2573758641_297d6d19fa_o.jpg" alt="#" className='img'/>
+    <img src={tour.image} alt="#" className='img'/>
         <Box paddingX={1}>
       <Typography variant='subtitle1' component='h2'>
-        Immerse into the falls
+        {tour.name}
       </Typography>
              <Box sx={{
       display:'flex',
@@ -20,7 +20,7 @@ const TourCard = () => {
       }}>
         <AccessTime sx={{width:12.5}}/>
         <Typography variant='body2' component='p' marginLeft={0.5}>
-          5 hours
+          {tour.duration} hours
         </Typography>
       </Box>
         <Box sx={{
@@ -31,15 +31,15 @@ const TourCard = () => {
           
           <Rating name="read-only" value={4.5} precision={0.5} readOnly size='small' />
           <Typography variant='body2' component='p' marginLeft={0.5}>
-            4.5
+            {tour.rating}
           </Typography>
              <Typography variant='body2' component='p' marginLeft={0.5}>
-            (2302)
+               ({tour.numberOfReviews}) reviews
           </Typography>
         </Box>
           <Box>
           <Typography variant='h6' component='h2'>
-            From $220
+            From ${tour.price}
           </Typography>
           </Box>
           </Box>
