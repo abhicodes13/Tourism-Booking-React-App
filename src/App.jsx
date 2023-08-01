1b405709bb0e4fc8d270f8c2219a7b0d0b67940d
abhicodes13 <68747686+abhicodes13@users.noreply.github.com>
2023-08-01 12:56:56 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import './App.css'
+import TourCard from './components/TourCard'
+
+
+export default function App() {
+  return (
+    <TourCard />
+  )
+}
---
+++ src/components/TourCard.jsx
@@ -0,0 +1,12 @@
+import Paper from '@mui/material/Paper';
+
+
+const TourCard = () => {
+  return (
+    <Paper elevation={1}>
+      Hello
+    </Paper>
+  )
+}
+
+export default TourCard
