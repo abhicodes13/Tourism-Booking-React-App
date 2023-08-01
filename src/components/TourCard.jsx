4d69216f12f6695ffb2b8ed2d8e11e81ee026e5f
abhicodes13 <68747686+abhicodes13@users.noreply.github.com>
2023-08-01 14:57:14 +0000

    Added Reviews & Ratings to TourCard Component

--- src/components/TourCard.jsx
+++ src/components/TourCard.jsx
@@ -2,11 +2,12 @@ import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
-
+import { AccessTime } from '@mui/icons-material';
+import Rating from '@mui/material/Rating';
 
 const TourCard = () => {
   return (
-    <Grid item xs={3}>
+    <Grid item xs={4}>
     <Paper elevation={1}>
     <img src="https://tcproduction.blob.core.windows.net/media/%7B240f8b72-1159-4fd3-a150-0a837f50ba4a%7D.2573758641_297d6d19fa_o.jpg" alt="#" className='img'/>
         <Box paddingX={1}>
@@ -17,10 +18,30 @@ const TourCard = () => {
       display:'flex',
       alignItems:'center'
       }}>
-        <Typography variant='body2' component='p'>
+        <AccessTime sx={{width:12.5}}/>
+        <Typography variant='body2' component='p' marginLeft={0.5}>
           5 hours
         </Typography>
       </Box>
+        <Box sx={{
+      display:'flex',
+      alignItems:'center'
+      }}
+           marginTop={3}>
+          
+          <Rating name="read-only" value={4.5} precision={0.5} readOnly />
+          <Typography variant='body2' component='p' marginLeft={0.5}>
+            4.5
+          </Typography>
+             <Typography variant='body2' component='p' marginLeft={0.5}>
+            (2302)
+          </Typography>
+        </Box>
+          <Box>
+          <Typography variant='h6' component='h2'>
+            From $220
+          </Typography>
+          </Box>
           </Box>
     </Paper>
       </Grid>
