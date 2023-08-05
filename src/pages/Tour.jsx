484f5beee3408fd8cea312d9b2da3534082c8150
abhicodes13 <68747686+abhicodes13@users.noreply.github.com>
2023-08-05 22:37:18 +0000

    Updated Tour Page

--- src/pages/Tour.jsx
+++ src/pages/Tour.jsx
@@ -1,9 +1,12 @@
 import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
 
 
 const Tour =()=>
   <Container sx={{width:900}}>
-    hello
+    <Typography>
+      Explore the world
+    </Typography>
   </Container>
 
 export default Tour;
