0e3c1a7980d94ce2492b51420683542967b9a693
abhicodes13 <68747686+abhicodes13@users.noreply.github.com>
2023-08-05 17:26:27 +0000

    Added Home Route

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
-import './App.css'
+import './App.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Home from './pages/Home';
+
 
 export default function App() {
   return (
-   <Home />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
